3f8930932c39f9bc951e5d282390b95bf057e2c0
MARZOOQUE <mmarzooque@gmail.com>
2023-06-14 18:47:04 +0530

    responsveness

--- src/Components/ErrorAlert/ErrorAlert.jsx
+++ src/Components/ErrorAlert/ErrorAlert.jsx
@@ -1,13 +1,36 @@
-import * as React from 'react';
-import Alert from '@mui/material/Alert';
-import Stack from '@mui/material/Stack';
+import * as React from "react";
+import Alert from "@mui/material/Alert";
+import Stack from "@mui/material/Stack";
+import { styled } from "@mui/material/styles";
+
+const Root = styled("div")(({ theme }) => ({
+  margin: "auto",
+  marginBottom: "10px",
+  height: "5px",
+  [theme.breakpoints.down("md")]: {
+    width: "50%",
+  },
+  [theme.breakpoints.down("sm")]: {
+    width: "80%",
+  },
+  [theme.breakpoints.up("md")]: {
+    width: "40%",
+  },
+  [theme.breakpoints.up("lg")]: {
+    width: "30%",
+  },
+}));
 
 export default function BasicAlerts(props) {
-    const { error, setError } = props;
+  const { error, setError } = props;
 
   return (
-    <Stack sx={{ width: '30%',  margin:"auto",marginBottom:"10px", height:"5px" }}>
-      <Alert severity="error"  onClose={() => setError("")}>{error}</Alert>
-    </Stack>
+    <Root>
+      <Stack>
+        <Alert severity="error" onClose={() => setError("")}>
+          {error}
+        </Alert>
+      </Stack>
+    </Root>
   );
 }
